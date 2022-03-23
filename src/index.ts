2e13f08d0c63c6ac6bf0969a73f886f2a89516fb
shiyiya <shiyiya.11@gmail.com>
2022-03-23 11:02:05 +0800

    .

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import { SoteContext } from './context'
+
 export { Provider } from './provider'
 export { connect } from './connect'
 export { createStore } from './store'
 export { useStore, useCommit, useSelector, useActions } from './hook'
+
+export const Consumer = SoteContext.Consumer
